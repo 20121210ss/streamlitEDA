28b316a2e1563c9bbaa4a9b9abbcab8b41a684ac
20121210ss <109130652+20121210ss@users.noreply.github.com>
2023-12-27 10:23:01 +0800

    Update tryStreamlit.py

--- tryStreamlit.py
+++ tryStreamlit.py
@@ -184,8 +184,7 @@ def display_messages(messages):
 def DataFrame():
     if st.session_state.df is not None:
         # The second return value is Mito generated code
-        # new_dfs, code = spreadsheet(st.session_state.df,key='df1')
-        new_dfs , code = spreadsheet(st.session_state.df,key='df1')
+        new_dfs, code = spreadsheet(st.session_state.df,key='df1')
         selection = spreadsheet(st.session_state.df,key='df2',return_type='selection')
         st.write(selection)
         # Display the code
