128838357ab023d758d6e4aa2398c31d198142b0
20121210ss <109130652+20121210ss@users.noreply.github.com>
2023-12-27 09:27:24 +0800

    Update tryStreamlit.py

--- tryStreamlit.py
+++ tryStreamlit.py
@@ -186,7 +186,7 @@ def DataFrame():
         # The second return value is Mito generated code
         # new_dfs, code = spreadsheet(st.session_state.df,key='df1')
         a , code = spreadsheet(st.session_state.df,key='df1')
-        selection = spreadsheet(key='df1',return_type=selection)
+        selection = spreadsheet(st.session_state.df,key='df1',return_type=selection)
         st.write(selection)
         # Display the code
         st.session_state.code=code
