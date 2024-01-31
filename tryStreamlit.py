d44a8a5541bb8d5dd0ff6267f091385e38eb0f09
20121210ss <109130652+20121210ss@users.noreply.github.com>
2024-01-31 10:54:14 +0800

    te

--- tryStreamlit.py
+++ tryStreamlit.py
@@ -195,9 +195,7 @@ def codePage():
 def refreshCode(code_placeholder):
     if st.session_state.inputCode is not None:
         try:
-            ans = exec(st.session_state.inputCode)
-            st.write("test:\n")
-            st.write(ans)
+            ans = eval(st.session_state.inputCode)
             st.session_state.outputCode = st.session_state.outputCode+"\n"+st.session_state.inputCode+"\n"+ans
         except:
             ans = "#無法執行"
