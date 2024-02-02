0e167595f300bd5150bbc28fd706a01011963ee7
20121210ss <109130652+20121210ss@users.noreply.github.com>
2024-02-02 11:13:16 +0800

    de

--- tryStreamlit.py
+++ tryStreamlit.py
@@ -120,8 +120,9 @@ def main():
                         visualPic(item[1],vs)
                 
                 hint = st.text_area("若覺得產圖不準確，可以輸入資料集的用途及特徵意義等，便於提升預測準確率")
+                st.text("test:"+hint)
+                st.write(hint)
                 if st.button("重新產圖"):
-                    st.write("test:"+hint)
                     if hint is not None:
                         rel = repredictThreePic(str(st.session_state.colList),key,hint)
                         st.session_state.ThreePicResult = regularResponse(rel)
