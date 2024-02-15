6b310deeafc2ec86df1e5f900b227d754ab0449e
20121210ss <109130652+20121210ss@users.noreply.github.com>
2024-02-15 11:44:04 +0800

    te

--- tryStreamlit.py
+++ tryStreamlit.py
@@ -204,11 +204,11 @@ def codePage():
     ans_placeholder.write("")
 
     if st.button("新增資料集變數(若程式碼中需使用資料集時點選)"):
-        st.session_state.inputCode = inputArea_placeholder.text_area("輸入自行撰寫python code","df")
-    if st.button("新增結果變數(若希望程式碼回傳結果時點選，以儲存並顯示欲回傳的結果)"):
-        st.session_state.inputCode = inputArea_placeholder.text_area("輸入自行撰寫python code","result")
-    
-    st.session_state.inputCode = inputArea_placeholder.text_area("輸入自行撰寫python code",)
+        st.session_state.inputCode = inputArea_placeholder.text_area("輸入自行撰寫python code",st.session_state.inputCode+"df")
+    elif st.button("新增結果變數(若希望程式碼回傳結果時點選，以儲存並顯示欲回傳的結果)"):
+        st.session_state.inputCode = inputArea_placeholder.text_area("輸入自行撰寫python code",st.session_state.inputCode+"result")
+    else :
+        st.session_state.inputCode = inputArea_placeholder.text_area("輸入自行撰寫python code",)
     
     if st.button("送出"):
         refreshCode(code_placeholder,ans_placeholder)
