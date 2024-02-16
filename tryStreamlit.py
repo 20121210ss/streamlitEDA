582000c3c66e76b8c24750e5ceb31870195f0d18
20121210ss <109130652+20121210ss@users.noreply.github.com>
2024-02-16 11:15:33 +0800

    re

--- tryStreamlit.py
+++ tryStreamlit.py
@@ -169,9 +169,10 @@ def main():
                             st.session_state.OneColresult = predictOneCol(st.session_state.selectCol,test,key)
                             st.session_state.OneColresult = regularResponse(st.session_state.OneColresult)
                             part = str(st.session_state.OneColresult[0]).split(":",1)
-                            part[1] = (str(part[1])).replace("""\n""","")
-                            st.write(part)
-                            st.session_state.OneColresult[0] = (f"{part[1]}")
+                            part = str(part[1]).split(",",1)
+                            part[0] = str(part[0]).replace("\n","")
+                            st.write(part[0])
+                            st.session_state.OneColresult[0] = (f"{part[0]}",st.session_state.OneColresult[0][1])
                             st.write(st.session_state.OneColresult[0])
                             for item in st.session_state.OneColresult:
                                 st.text(str(item[0]).replace(":"," "))
