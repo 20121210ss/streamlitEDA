630d4cb4a515753f4193d2a05ee9825684f29896
20121210ss <109130652+20121210ss@users.noreply.github.com>
2024-03-05 16:02:28 +0800

    try

--- preprocessing.py
+++ preprocessing.py
@@ -5,7 +5,14 @@ import pandas as pd
 import allVariable
 
 def preprocessing():
-    st.tabs(['資料集前處理'])
+    col1, col2 = st.columns(spec=[0.7,0.3])
+    with col1:
+        st.tabs(['資料集'])
+        st.data_editor(allVariable.df)
+    with col2:
+        st.tabs(['前處理動作'])
+        if st.button("刪除遺漏值"):
+            st.warning("ok")
     
 if allVariable.df is not None:
     preprocessing()
